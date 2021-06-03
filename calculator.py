7a28b6e0aa640fc9b762ec04a3101aa124ffdfbc
Casey Nord <nordcw@gmail.com>
2021-06-02 17:43:28 -0700

    bringing calculator app over from circle ci repo

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""
+CS362 - Calculator App for Continuous Integration
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
